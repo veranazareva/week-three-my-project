497dbfbd0eb3096d2355b082411b2a3674ea3d35
veranazareva <vera_nazareva@rambler.ru>
2023-02-16 19:46:38 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 
 import './App.css';
 import { Books } from './Books';
-import books from './books.jpg';
 import { BooksEnd } from './BooksEnd';
-import booksTwo from './booksTwo.jpg';
+import books from './books.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      
       <div>
         <h1>Книги для чтения в феврале</h1>
       </div>
-
       <div className='listOfBooks'>
-        <div>
-          <h2>Прочитать обязательно</h2>
-          <Books />
-        </div>
-        <div className='capture'>
-          <img src={ books } alt="books" width="400px" />
-        </div>
+        <h2>Прочитать обязательно</h2>
+        <Books />
       </div>
-
       <div className='listOfBooks'>
-        <div className='capture'>
-          <img src={ booksTwo } alt="booksTwo" width="400px" />
-        </div>
-        <div>
-          <h2>Прочитать дополнительно</h2>
-          <BooksEnd />
-        </div>
+        <h2>Прочитать дополнительно</h2>
+        <BooksEnd />
       </div>
-
+      <div className='capture'>
+        <img src={ books } alt="books" width="70%" />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
